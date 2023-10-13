4c3047e5b381b1fa44f885de7302a7cff2060865
Sean Vargas <svarg1@unh.newhaven.edu>
2023-10-13 15:46:42 -0400

    Create bar plots for Europe and Southern Europe

--- SeanCode/dataExploration.py
+++ SeanCode/dataExploration.py
@@ -1,11 +1,6 @@
-import datetime
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import missingno as msno
-from sklearn.cluster import KMeans
-from sklearn.preprocessing import StandardScaler
 
 countries = ["Sweden", "Turkey", "Romania", "Czech Republic", "Norway", "Italy", "Austria", "Netherlands", "Poland", "Switzerland", \
 			 "France", "Finland", "Ukraine", "United Kingdom", "Denmark", "Germany", "Portugal", "Belgium"]
@@ -23,31 +18,31 @@ trends = trends.rename(columns = {"week":"Year-Month"})
 trends.columns = ["Score", "Year-Month", "Rank", "Country", "Sub-Region", "Term"]
 trends = trends[["Term", "Rank", "Country", "Sub-Region", "Year-Month", "Score"]]
 
-# Create dataframes for Eurpean countries
+# Create dataframes for European countries
 europeTrends = trends[trends["Country"].isin(countries)]
-nordicTrends = trends[trends["Country"].isin(nordicCountries)]
-eastEuroTrends = trends[trends["Country"].isin(eastEuroCountries)]
-southEuroTrends = trends[trends["Country"].isin(southEuroCountries)]
-centralEuroTrends = trends[trends["Country"].isin(centralEuroCountries)]
-westEuroTrends = trends[trends["Country"].isin(westEuroCountries)]
-swedenTrends = trends[trends["Country"] == "Sweden"]
-turkeyTrends = trends[trends["Country"] == "Turkey"]
-romaniaTrends = trends[trends["Country"] == "Romania"]
-czechTrends = trends[trends["Country"] == "Czech Republic"]
-norwayTrends = trends[trends["Country"] == "Norway"]
-italyTrends = trends[trends["Country"] == "Italy"]
-austriaTrends = trends[trends["Country"] == "Austria"]
-dutchTrends = trends[trends["Country"] == "Netherlands"]
-polandTrends = trends[trends["Country"] == "Poland"]
-swissTrends = trends[trends["Country"] == "Switzerland"]
-frenchTrends = trends[trends["Country"] == "France"]
-finnishTrends = trends[trends["Country"] == "Finland"]
-ukrainianTrends = trends[trends["Country"] == "Ukraine"]
-britishTrends = trends[trends["Country"] == "United Kingdom"]
-danishTrends = trends[trends["Country"] == "Denmark"]
-germanTrends = trends[trends["Country"] == "Germany"]
-portugalTrends = trends[trends["Country"] == "Portugal"]
-belgianTrends = trends[trends["Country"] == "Belgium"]
+nordicTrends = europeTrends[europeTrends["Country"].isin(nordicCountries)]
+eastEuroTrends = europeTrends[europeTrends["Country"].isin(eastEuroCountries)]
+southEuroTrends = europeTrends[europeTrends["Country"].isin(southEuroCountries)]
+centralEuroTrends = europeTrends[europeTrends["Country"].isin(centralEuroCountries)]
+westEuroTrends = europeTrends[europeTrends["Country"].isin(westEuroCountries)]
+swedenTrends = europeTrends[europeTrends["Country"] == "Sweden"]
+turkeyTrends = europeTrends[europeTrends["Country"] == "Turkey"]
+romaniaTrends = europeTrends[europeTrends["Country"] == "Romania"]
+czechTrends = europeTrends[europeTrends["Country"] == "Czech Republic"]
+norwayTrends = europeTrends[europeTrends["Country"] == "Norway"]
+italyTrends = europeTrends[europeTrends["Country"] == "Italy"]
+austriaTrends = europeTrends[europeTrends["Country"] == "Austria"]
+dutchTrends = europeTrends[europeTrends["Country"] == "Netherlands"]
+polandTrends = europeTrends[europeTrends["Country"] == "Poland"]
+swissTrends = europeTrends[europeTrends["Country"] == "Switzerland"]
+frenchTrends = europeTrends[europeTrends["Country"] == "France"]
+finnishTrends = europeTrends[europeTrends["Country"] == "Finland"]
+ukrainianTrends = europeTrends[europeTrends["Country"] == "Ukraine"]
+britishTrends = europeTrends[europeTrends["Country"] == "United Kingdom"]
+danishTrends = europeTrends[europeTrends["Country"] == "Denmark"]
+germanTrends = europeTrends[europeTrends["Country"] == "Germany"]
+portugalTrends = europeTrends[europeTrends["Country"] == "Portugal"]
+belgianTrends = europeTrends[europeTrends["Country"] == "Belgium"]
 
 # Assign region classifiers to countries
 europeTrends = europeTrends.assign(Region = europeTrends["Country"])
@@ -56,3 +51,19 @@ europeTrends.loc[europeTrends["Region"].isin(eastEuroCountries), "Region"] = "Ea
 europeTrends.loc[europeTrends["Region"].isin(southEuroCountries), "Region"] = "Southern Europe"
 europeTrends.loc[europeTrends["Region"].isin(centralEuroCountries), "Region"] = "Central Europe"
 europeTrends.loc[europeTrends["Region"].isin(westEuroCountries), "Region"] = "Western Europe"
+
+# Plot number of searches by region
+europeTrendsCount = europeTrends.groupby("Region", as_index=False).count()
+sns.barplot(x="Region", y="Term", data=europeTrendsCount)
+plt.xlabel("")
+plt.ylabel("Searches")
+plt.title("Number of Searches by Region")
+plt.show()
+
+# Plot number of searches in Southern Europe by country
+southEuroTrendsCount = southEuroTrends.groupby("Country", as_index=False).count()
+sns.barplot(x="Country", y="Term", data=southEuroTrendsCount)
+plt.xlabel("")
+plt.ylabel("Searches")
+plt.title("Number of Searches by Southern European Country")
+plt.show()
