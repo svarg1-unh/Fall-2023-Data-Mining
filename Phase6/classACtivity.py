ea2b655d7df9c63a85fd777fc398f3e369f5e3d9
Sean Vargas <svarg1@unh.newhaven.edu>
2023-11-26 23:30:00 -0500

    Working algorithm

--- Phase6/classACtivity.py
+++ Phase6/classACtivity.py
@@ -1,53 +1,27 @@
 # Import necessary libraries
 import pandas as pd
-from IPython.display import display
-from sklearn.model_selection import train_test_split
-from sklearn.svm import SVC
-from sklearn.metrics import accuracy_score, classification_report
+import seaborn as sns
+import matplotlib.pyplot as plt
+from apyori import apriori
 
 # Data Preprocessing
 file_path = 'trends_by_cat.csv'
 df = pd.read_csv(file_path)
-df = df[df["location"] == "Global"]
+groupedDf = df.groupby("location")["category"].unique().apply(list)
+categories = groupedDf.tolist()
 
-df["frequency"] = df["query"].map(df["query"].value_counts())
-print(df)
-#print(len(pd.unique(df["query"])))
+rules = apriori(groupedDf, min_support=0.10, min_confidence=0.60, min_lift=2, min_length = 2)
+results = list(rules)
 
+# Extract support and confidence for plot
+suppAndConf = [[None for j in range(2)] for i in range(len(results))]
+for i in range(0, len(results)):
+    suppAndConf[i][0] = results[i][1]
+    suppAndConf[i][1] = results[i][2]
+for i in range(0, len(suppAndConf)):
+    suppAndConf[i][1] = suppAndConf[i][1][0][2]
+plotDf = pd.DataFrame(suppAndConf)
+plotDf.columns = ["support", "confidence"]
 
-
-
-
-
-
-
-
-# # Move category to last column
-# i = list(df.columns)
-# a, b = i.index("category"), i.index("query")
-# i[b], i[a] = i[a], i[b]
-# df = df[i]
-
-# # Assuming the last column is the target variable (category) and the rest are features
-# X = df["rank"]  # Features
-# y = df["category"]  # Category
-
-# # Split the data into training and testing sets
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-# # Create an SVM model
-# svm_model = SVC(kernel='linear')
-
-# # Train the model on the training data
-# svm_model.fit(X_train, y_train)
-
-# # Make predictions on the test data
-# y_pred = svm_model.predict(X_test)
-
-# # Evaluate the model
-# accuracy = accuracy_score(y_test, y_pred)
-# print(f"Accuracy: {accuracy:.2f}")
-
-# # Print a classification report for more detailed metrics
-# print("Classification Report:")
-# print(classification_report(y_test, y_pred))
+sns.scatterplot(data=plotDf, x="support", y="confidence")
+plt.show()  
